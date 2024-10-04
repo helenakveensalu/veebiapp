3ebe78f3c5bd7e34dd6df12202e4f2b504f26f39
helenakveensalu <helena.k.veensalu@gmail.com>
2024-10-04 18:47:51 +0200

    Loeng 2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const ejs = require('ejs')
+const path = require("path")
 
 const app = express()
 
@@ -10,10 +12,12 @@ function tervita(req, res) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/static/style.css">
 </head>
         <body>
             <h1>Tere, ${req.query.nimi}</h1>
             <p>Kes hommikuti külas käib, see asjatult ei longi</p>
+            <img class="koer" src="static/asset/koer.jpg">
         </body>
         </html>
         `)
@@ -37,6 +41,40 @@ function parimkoer (req, res) {
         `)
 }
 
+const matk1 = {
+    nimetus: "Sügismatk Kõrvemaal",
+    pildiUrl: "/static/asset/koer.jpg",
+    kirjeldus: "Lähme ja oleme 3 päeva Kõrvemaal",
+    osalejad: ["üksinimene@gmail.com", "kaksinimene@gmail.com"]
+}
+
+const matk2 = {
+    nimetus: "Kevadine matk Hiiumaal",
+    pildiUrl: "/static/asset/koer.jpg",
+    kirjeldus: "Lähme oleme paar päeva Hiiumaal",
+    osalejad: ["üksonimenex@gmail.com"]
+}
+
+const matkad = [matk1, matk2]
+
+function palavTervitus(req, res) {
+    const parameetrid = { isik: req.query.nimi }
+    res.render("palavtervitus", parameetrid)
+}
+
+function naitaMatkad(req, res) {
+    res.render("matkad", {matkad: matkad})
+}
+
+app.use('/static', express.static('public'))
+app.use(express.json())
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get('/Finley', parimkoer)
+
+app.get('/palavtervitus', palavTervitus)
+app.get('/matkad', naitaMatkad)
 
 app.listen(3000)
